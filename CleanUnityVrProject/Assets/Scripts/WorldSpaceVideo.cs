c14c908c03fd91ac062f616b99e958887b8e60e9
Edd <edd.tallentire@gmail.com>
2019-07-22 01:49:10 +0100

    Added menu for play and pause, incorporating the shooting aspect to trigger the pause button which will change to vive controller later, new textures and materials for these buttons and created 2 new scripts for play \& pause and for shooting the pause button

---
+++ CleanUnityVrProject/Assets/Scripts/WorldSpaceVideo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WorldSpaceVideo : MonoBehaviour {
+
+    public Material playButtonMaterial;
+    public Material pauseButtonMaterial;
+    public Renderer screenRenderer;
+
+    private VideoPlayer videoPlayer;
+
+
+    void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayPause()
+    {
+        if (videoPlayer.isPlaying){
+            videoPlayer.Pause();
+            screenRenderer.material = playButtonMaterial;
+        } else
+        {
+            videoPlayer.Play();
+            screenRenderer.material = pauseButtonMaterial;
+        }
+    }
+}
